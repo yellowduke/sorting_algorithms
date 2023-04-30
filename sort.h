f45acd79b5e95e94c0c95480f846ae327a7d87b6
yellowduke <yelohduke@gmail.com>
2023-04-29 21:26:56 -0700

    initial commit

--- sort.h
+++ sort.h
@@ -34,7 +34,7 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
-void _ssort(int *a, int size, int n);;
+void _ssort(int *a, int size, int n);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 #endif
